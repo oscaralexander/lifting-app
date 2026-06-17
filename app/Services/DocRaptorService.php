33c0c25371789d416674d14a2bc2ea43876cffca
Alexander Griffioen <alex.griffioen@gmail.com>
2026-06-17 12:12:53 +0200

    Enforce testmode

--- app/Services/DocRaptorService.php
+++ app/Services/DocRaptorService.php
@@ -13,7 +13,7 @@ class DocRaptorService
             ->post(config('services.docraptor.url'), array_merge([
                 'document_content' => $html,
                 'type' => 'pdf',
-                'test' => ! app()->environment('production'),
+                'test' => true, // ! app()->environment('production'),
             ], $options));
 
         return $response->body();
